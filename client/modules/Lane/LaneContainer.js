9285fa97475db4f2f43cafa16002876d8177d3bb
Elżbieta Kukułka <ellakaras@gmail.com>
2018-07-07 17:51:16 +0200

    Add lane actions, lane reducers, note actions, note reducers

---
+++ client/modules/Lane/LaneContainer.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import Lane from './Lane';
+import * as laneActions from './LaneActions';
+import { createNote } from '../Note/NoteActions';
+import { deleteLane, updateLane, editLane } from './LaneActions';
+import { createNoteRequest } from '../Note/NoteActions';
+
+const mapStateToProps = (state, ownProps) => ({
+  laneNotes: ownProps.lane.notes.map(noteId => state.notes[noteId])
+});
+
+const mapDispatchToProps = {
+  editLane,
+  deleteLane,
+  updateLane,
+  addNote: createNoteRequest,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Lane);
